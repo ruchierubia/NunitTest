d4e4414e2e0f5c81d94a7fa8ab3e0e309cbb3859
Ruchie V. Rubia <rrubia@fullscale.io>
2020-03-04 19:12:57 +0800

    Add project files.

---
+++ bank/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class Account
+    {
+        private float balance;
+        private float minBalance = 10;
+        public Account()
+        {
+
+        }
+
+        public Account(int value)
+        {
+            balance = value;
+        }
+
+        public void Deposit(float amount)
+        {
+            balance += amount;
+        }
+
+        public void Withraw(float amount)
+        {
+            balance -= amount;
+        }
+
+        public Account TransferMinFunds(Account distination, float amount)
+        {
+            if (Balance - amount > MinBalance)
+            {
+                distination.Deposit(amount);
+                Withraw(amount);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+            return distination;
+        }
+
+        public void TransferFunds(Account  destination, float amount)
+        {
+            destination.Withraw(amount);
+            Withraw(amount);
+        }
+
+
+
+        public float Balance
+        {
+            get { return balance; }
+        }
+
+        public float MinBalance
+        {
+            get { return minBalance; }
+        }
+
+
+    }
+}
